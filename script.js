82edc67412ac66b04fb517bed43706cd7d48656d
Your Name <y.y.oleksander@gmail.com>
2023-06-07 16:57:30 +0300

    first

---
+++ script.js
@@ -0,0 +1,53 @@
+const playBoard = document.querySelector('.play-board');
+
+let foodX, foodY;
+let snakeBody = [];
+let snakeX = 5,
+  snakeY = 10;
+let vectorX = 0,
+  vectorY = 0;
+
+const changeFoodPosition = () => {
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
+};
+
+const changeDirection = (e) => {
+  if (e.key === 'ArrowUp') {
+    vectorX = 0;
+    vectorY = -1;
+  } else if (e.key === 'ArrowDown') {
+    vectorX = 0;
+    vectorY = 1;
+  } else if (e.key === 'ArrowRight') {
+    vectorX = 1;
+    vectorY = 0;
+  } else if (e.key === 'ArrowLeft') {
+    vectorX = -1;
+    vectorY = 0;
+  }
+};
+
+const initGame = () => {
+  let htmlMarkup = `<div class="food" style="grid-area: ${foodY} / ${foodX}" ></div>`;
+  if (snakeX === foodX && snakeY === foodY) {
+    changeFoodPosition();
+    snakeBody.push([foodX, foodY]);
+    console.log(snakeBody);
+  }
+
+  snakeBody[0] = [snakeX, snakeY];
+  snakeX += vectorX;
+  snakeY += vectorY;
+
+  for (let i = 0; i < snakeBody.length; i++) {
+    htmlMarkup += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}" ></div>`;
+  }
+
+  playBoard.innerHTML = htmlMarkup;
+};
+
+changeFoodPosition();
+
+document.addEventListener('keydown', changeDirection);
+setInterval(initGame, 125);
